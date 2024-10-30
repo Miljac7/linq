ea1bba01a775490279117a9b21b970e45cd5e005
Miljac7 <152974534+Miljac7@users.noreply.github.com>
2024-10-30 09:16:02 +0100

    Add files via upload

---
+++ linq Metoda/linq Metoda/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_Metoda
+{
+    internal class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+
+        public override string ToString()
+        {
+            string ispis = "Redni Broj:" + this.StudentID + "Ime:" + this.StudentName;
+            return ispis;
+        }
+    }
+}
